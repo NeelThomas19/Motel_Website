4a4a763655f5dcc3f54444280983e562df85ded2
Neel Thomas <20cs095@charusat.edu.in>
2023-06-04 12:52:30 +0530

    update

--- src/Components/Overview.jsx
+++ src/Components/Overview.jsx
@@ -1,28 +1,86 @@
 import React from 'react'
 import { FaParking, FaSnowflake } from "react-icons/fa";
 import { BiWifi } from "react-icons/bi";
-import { Ri24HoursLine } from "react-icons/ri"
+import { Ri24HoursLine } from "react-icons/ri";
 
 const Overview = () => {
   return (
      <div id='Overview' className='overview'>
-      <div className="text">
-        <h4>SeaRay Motel</h4>
-        <p><a href='#'>Reviews</a></p>
-        <h5>Some Facilities</h5>
-        <div className="text1">
-        <p className='t1'><FaParking />Parking Included</p>
-        <p><FaSnowflake />Air Conditioning</p>
+      <div class="row w-100">
+  <div class="col-sm-6 mb-3 mb-sm-0">
+    <div class="cd">
+      <div class="card-body">
+        <h5 class="card-title">Facilities Provided</h5>
+        <div class="d-flex flex-row mb-3 grid gap-5 row-gap-3">
+        <p class="card-text"><FaParking/>Parking Included</p>
+        <p class="card-text"><FaSnowflake/>Air Conditioning</p>
         </div>
-        <div className="text1">
-          <p><BiWifi />Free WiFi</p>
-          <p><Ri24HoursLine />24/7 Front Desk</p>
+        <div class="d-flex flex-row mb-3 grid gap-5 row-gap-3">
+        <p class="card-text"><Ri24HoursLine />24/7 Front Desk</p>
+        <p class="card-text"><BiWifi/>Free WiFi</p>
         </div>
+        {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
       </div>
-      <div className="expl">
-        <h4 className='Explore'>Explore the area!</h4>
-        <div className="text1">
+    </div>
+  </div>
+  <div class="col-sm-6" id='explore'>
+    <div class="cd">
+      <div class="card-body">
+        <h5 class="card-title" >Explore the area!</h5>
+        <div class="container text-center">
+          <div class="row">
+            <div class="col">
+            <p class="card-text">Wildwood Boardwalk</p>
+            </div>
+            <div class="col">
+            <p class="card-text">2 min walk</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+            <p class="card-text">Wildwood Beach</p>
+            </div>
+            <div class="col">
+            <p class="card-text">5 min walk</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+            <p class="card-text">Morey's Piers</p>
+            </div>
+            <div class="col">
+            <p class="card-text">10 min walk</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+            <p class="card-text">Cape May, NJ(WWD-Cape May County)</p>
+            </div>
+            <div class="col">
+            <p class="card-text">15 min drive</p>
+            </div>
+          </div>
+        {/* <div class="d-flex flex-row mb-3 grid gap-5 row-gap-1 ">
+        <p class="card-text">Wildwood Boardwalk</p>
+        <p class="card-text">2 min walk</p>
         </div>
+        <div class="d-flex flex-row mb-3 grid gap-5 row-gap-1 ">
+        <p class="card-text">Wildwood Beach</p>
+        <p class="card-text">5 min walk</p>
+        </div>
+        <div class="d-flex flex-row mb-3 grid gap-5 row-gap-1 ">
+        <p class="card-text">Morey's Piers</p>
+        <p class="card-text">10 min walk</p>
+        </div>
+        <div class="d-flex flex-row mb-3 grid gap-5 row-gap-1 ">
+        <p class="card-text">Cape May, NJ(WWD-Cape May County)</p>
+        <p class="card-text">15 min drive</p>
+        </div> */}
+        {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+      </div>
+    </div>
+  </div>
+</div>
       </div>
       </div>
   )
